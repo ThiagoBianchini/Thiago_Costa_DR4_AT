1fb96c8f26844ef1bbf8136d9750c5590bad8d77
Thiago Bianchini <105920702+ThiagoBianchini@users.noreply.github.com>
2024-12-04 11:04:15 +0000

    aaa

--- src/Exercicios/ex8.jsx
+++ src/Exercicios/ex8.jsx
@@ -1,38 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Ex8() {
-
-  // Definindo o componente Botao
   function Botao({ texto, cor, funcao, desativado = false }) {
     return (
       <button
         className="btn"
         style={{
           backgroundColor: cor,
-          cursor: desativado ? 'not-allowed' : 'pointer',
+          cursor: desativado ? "not-allowed" : "pointer",
         }}
         onClick={!desativado ? funcao : null}
-        disabled={desativado}>
+        disabled={desativado}
+      >
         {texto}
       </button>
     );
   }
 
-  // Estado para controlar a cor da mensagem
-  const [corMensagem, setCorMensagem] = useState('black'); 
+  const [corMensagem, setCorMensagem] = useState("black");
 
-  // Função para mudar a cor da mensagem
   const mudarCorMensagem = () => {
-    const cores = ['red', 'green', 'blue', 'orange', 'purple'];
+    const cores = ["red", "green", "blue", "orange", "purple"];
     const corAtual = corMensagem;
     let novaCor = cores[Math.floor(Math.random() * cores.length)];
 
-    // Garantir que a nova cor não seja a mesma da anterior
     while (novaCor === corAtual) {
       novaCor = cores[Math.floor(Math.random() * cores.length)];
     }
 
-    setCorMensagem(novaCor);  
+    setCorMensagem(novaCor);
   };
 
   return (
@@ -41,7 +37,8 @@ function Ex8() {
       <Botao
         texto="Mudar cor"
         cor="blue"
-        funcao={mudarCorMensagem}  // Passando a função que altera a cor
+        funcao={mudarCorMensagem}
+        desativado={false}
       />
     </div>
   );
